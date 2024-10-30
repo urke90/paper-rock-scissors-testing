0b0f287ae3ef46795b00530dd44fef5e9063cd47
urke90 <urosbijelic90@gmail.com>
2024-10-30 14:15:17 +0100

    create playRound function with logic for showing winner log messages and score incrementing

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  const userInput = prompt('Choose between paper, rock and scissors');
+  const userInput = prompt(
+    'Choose between paper, rock and scissors'
+  ).toLowerCase();
+
+  console.log('userInput', userInput);
 
   if (
     userInput !== 'paper' &&
@@ -28,4 +32,46 @@ function getHumanChoice() {
   return userInput;
 }
 
-getHumanChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      console.log('You Lost, Scissors beats Paper');
+      computerChoice++;
+    } else if (computerChoice === 'paper') {
+      console.log('Tied');
+    } else {
+      console.log('You won, Paper beats Rock');
+      humanChoice++;
+    }
+  }
+
+  if (humanChoice === 'rock') {
+    if (computerChoice === 'scissors') {
+      console.log('You won, Rock beats Scissors');
+      humanChoice++;
+    } else if (computerChoice === 'paper') {
+      console.log('You lost, Paper beats Rock');
+      computerChoice++;
+    } else {
+      console.log('Tied');
+    }
+  }
+
+  if (humanChoice === 'scissors') {
+    if (computerChoice === 'scissors') {
+      console.log('Tied');
+    } else if (computerChoice === 'paper') {
+      console.log('You won, Scissors beats Paper');
+      humanChoice++;
+    } else {
+      console.log('You lost, Rock beats Scissors');
+      computerScore++;
+    }
+  }
+}
+
+console.log({ humanScore });
+console.log({ computerChoice });
